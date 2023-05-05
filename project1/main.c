ec1f4be20937eef221efc9cb5c3c44e3df6febe5
mahmoud <mankimo155@gmail.com>
2023-05-05 12:51:07 +0300

    first commit

---
+++ project1/main.c
@@ -0,0 +1,22 @@
+/*
+ * main.c
+ *
+ *  Created on: May 4, 2023
+ *      Author: Lenovo
+ */
+#include<avr/io.h>
+#include<util/delay.h>
+int main(void)
+{
+	DDRA= 0b00011111 ;
+	while(1)
+	{
+		PORTA = 0b00000001 ;
+	    for(int i =0 ; i<5 ; i++)
+	    {
+
+	    	_delay_ms(100);
+	    	PORTA <<= 1;
+	    }
+	}
+}
